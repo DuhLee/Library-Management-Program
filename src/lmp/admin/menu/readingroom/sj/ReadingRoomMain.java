1add224b376e2dbc5425bd21315784a1025ce07f
sj_92 <sj_92@DESKTOP-2822HVG>
2022-12-19 22:02:07 +0900

    .

--- src/lmp/admin/menu/readingroom/sj/ReadingRoomMain.java
+++ src/lmp/admin/menu/readingroom/sj/ReadingRoomMain.java
@@ -1,27 +1,6 @@
 package lmp.admin.menu.readingroom.sj;
 
-import java.awt.CardLayout;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.sql.SQLException;
-import java.util.ArrayList;
-
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.border.LineBorder;
-
-import lmp.db.dao.SeatUseDetailDao;
-import lmp.db.vo.SeatUseDetailVO;
-
-public class ReadingRoomMain extends JFrame {
+public class ReadingRoomMain {
 
 	public ReadingRoomMain() {
 
